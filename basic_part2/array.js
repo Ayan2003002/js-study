aa4f9f456732b87740418fdb1e3115f6649a72cb
Akgiri <130314567+Ayan2003002@users.noreply.github.com>
2025-03-07 09:31:43 +0000

    Commit array.js

---
+++ basic_part2/array.js
@@ -0,0 +1,24 @@
+// Array 
+ const array1 = [1,2,3,4,5];
+
+ const array2 = new Array('ayan','jeet', 'kumar');
+
+ console.log(array2[1]);
+
+
+//  Array Method 
+array1.push(6);
+array1.push('akgiri');
+
+console.log(array1);
+array1.pop('akgiri')
+
+
+console.log(array1.includes(9));
+console.log(array1.indexOf(3));
+
+const myarray = array1.join();
+console.log(myarray);
+console.log(array1);
+
+
